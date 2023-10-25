85ddf59892be5e1245bcac2ba591a3835bd61171
Taahia23 <taahiatahsin@gmail.com>
2023-10-25 20:19:56 +0600

    send brand data in the server by post method

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.send(result)
+      res.send(result);
     })
 
     // addtocart
